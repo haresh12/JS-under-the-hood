edacbe4c01d7cd63a7c1baa51418fcb5b7d33932
haresh <lakhwaniharesh134@gmail.com>
2023-08-15 18:36:02 +0530

    Day 1: write down the topics

---
+++ DeepJavaScriptFoundations/Content.js
@@ -0,0 +1,15 @@
+/**
+ *  In folder DeepJavascript foundatation  we are starting we javascript under the wood and we try to learn some of the weird part
+ *  of javascript and here we are basically going to cover 4 topics as below
+ *    
+ *   Types  
+ *   Coercion
+ *   Equality
+ *   Static Typing
+ *   Scope
+ *   Scope and function expression
+ *   Advance scope
+ *   Closure
+ *   Objects
+ *   Prototypes
+ */
